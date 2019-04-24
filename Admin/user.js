edfd78daa57a46a51d8e7613579bc7d396ea2d41
Tega Oke <oketegah@gmail.com>
2019-04-24 15:13:03 +0100

    Removed redundant else statement

--- Admin/user.js
+++ Admin/user.js
@@ -1,10 +1,10 @@
 let db = require("../database");
 let autoincrement = require("../autoincrement");
 let response = "";
-function User(name, password, role) {
+
+function User(name, password) {
     this.name = name;
     this.password = password;
-    this.role = role;
 }
 
 User.prototype.createAccount = function(id) {
@@ -15,11 +15,9 @@ User.prototype.createAccount = function(id) {
         response = "Your admin account has been successfully created";
         return db.Admin;
     }
-    else {
-        db.Voters.push({id : userId, name : this.name, password : this.password, isAdmin : false});
-        response = "Your user account has been successfully created";
-        return db.Voters;
-    }
+    db.Voters.push({id : userId, name : this.name, password : this.password, isAdmin : false});
+    response = "Your user account has been successfully created";
+    return db.Voters;
 };
 
 module.exports = User;
