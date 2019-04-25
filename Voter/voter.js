534c2b3b9bd8adccaa4972c8824c3cb05ec90441
Tega Oke <oketegah@gmail.com>
2019-04-25 15:24:16 +0100

    Initialized voting function

--- Voter/voter.js
+++ Voter/voter.js
@@ -9,4 +9,8 @@ function Voter(name, password, role) {
 Voter.prototype = Object.create(User.prototype);
 Voter.prototype.constructor = Voter;
 
+Voter.prototype.vote = function(eventName, contestant, voteAmount) {
+    db.Events.forEach(value => value === "Fashion");
+};
+
 module.exports = Voter;
