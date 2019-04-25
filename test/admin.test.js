aeabde0414ad39a90e3dd1798f0b1f602e8b1b2d
Tega Oke <oketegah@gmail.com>
2019-04-25 07:31:11 +0100

    Tested if admin can create an event

--- test/admin.test.js
+++ test/admin.test.js
@@ -9,4 +9,8 @@ describe("Create Account", () => {
     test("Should check if admin can create account", () => {
         expect(newAdmin.createAccount()).toEqual({ id: 1, name: 'mike', password: '123', isAdmin: true });
     });
+    test("Should check if admin can create event", () => {
+        expect(newAdmin.createEvent("Fashion", 100, "tolu", "mike"))
+            .toEqual({ id: 1, name: 'Fashion', voteAmount: 100, contestants: [ 'tolu', 'mike' ] })
+    });
 });
