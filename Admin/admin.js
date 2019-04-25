2f00523ed346f1c8cb3cb913f068f09463f76b48
Tega Oke <oketegah@gmail.com>
2019-04-25 09:25:28 +0100

    Admin can now delete events

--- Admin/admin.js
+++ Admin/admin.js
@@ -18,5 +18,22 @@ Admin.prototype.createEvent = function(name, voteAmount, ...contestants) {
     console.log("Event successfully created");
     return {id : id, name : name, voteAmount : voteAmount, contestants : contestants};
 };
+Admin.prototype.deleteEvent = function() {
+    let indexOfEvent = db.Events.findIndex(value => value.name === 'Fashion' );
+    db.Events.splice(indexOfEvent, 1);
+    console.log(db.Events);
+    return "Event successfully deleted";
+};
+
+let newAdmin = new Admin("mike", "123", "admin");
+console.log(newAdmin.createAccount());
+
+let newAdmin2 = new Admin("mikey", "1234", "admin");
+console.log(newAdmin2.createAccount());
+
+console.log(newAdmin.createEvent("Fashion", 100, "Clothes"));
+console.log(newAdmin2.createEvent("Food", 50, "Rice"));
+
+console.log(newAdmin2.deleteEvent());
 
 module.exports = Admin;
