2fd1d2610001f6a21daf9068b9e5f80a1520042e
Tega Oke <oketegah@gmail.com>
2019-04-25 17:07:27 +0100

    Voter can noe vote

--- Voter/voter.js
+++ Voter/voter.js
@@ -1,5 +1,6 @@
 let db = require("../database");
 let User = require("../Admin/user");
+let Admin = require("../Admin/admin");
 
 function Voter(name, password, role) {
     User.call(this, name, password);
@@ -10,7 +11,19 @@ Voter.prototype = Object.create(User.prototype);
 Voter.prototype.constructor = Voter;
 
 Voter.prototype.vote = function(eventName, contestant, voteAmount) {
-    db.Events.forEach(value => value === "Fashion");
+    let contestantsName = db.Events.filter(value => value.name === eventName);
+    let conIndex = contestantsName[0].contestants.indexOf(contestant);
+    contestantsName[0].currentVotes[conIndex] = contestantsName[0].currentVotes[conIndex] + voteAmount;
+    return contestantsName;
 };
+
+let newAdmin = new Admin("tega", "123", "admin");
+console.log(newAdmin.createEvent("Fashion", "tolu", "mike"));
+
+let newVoter = new Voter("mike", "123", "voter");
+let voter2 = new Voter("tolu", "1", "voter");
+newVoter.vote("Fashion", "mike", 10);
+
+console.log(voter2.vote("Fashion", "tolu", 20));
 
 module.exports = Voter;
